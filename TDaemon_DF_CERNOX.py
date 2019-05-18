f3d07144f799f77aba0e5ea811b3aaba3a154e4b
Huang Junye <h.jun.ye@gmail.com>
2019-05-18 18:11:09 +0800

    TDaemon_DF_Cernox.py: adapt attributes and variable names to PEP8 styles

--- TDaemon_DF_CERNOX.py
+++ TDaemon_DF_CERNOX.py
@@ -34,281 +34,281 @@ class TControl():
 
 	def __init__(self):
 
-		self.PicoVisa = visa_subs.InitializeGPIB(20,0,query_delay="0.04")
-		self.PicoVisa.write("HDR0")
-		self.PicoVisa.write("ARN 1")
-		self.PicoVisa.write("REM 1")
-		self.TCSVisa = visa_subs.InitializeSerial("ASRL6::INSTR",idn="ID?")
+		self.pico_visa = visa_subs.initialize_gpib(20,0,query_delay="0.04")
+		self.pico_visa.write("HDR0")
+		self.pico_visa.write("ARN 1")
+		self.pico_visa.write("REM 1")
+		self.tcs_visa = visa_subs.initialize_serial("ASRL6::INSTR",idn="ID?")
 
 		address = ('localhost',18871)
-		self.Server = socket_utils.SockServer(address)
+		self.server = socket_utils.SockServer(address)
 
-		self.Resistance = 1.0
-		self.Temperature = 0.0
-		self.DeltaTemp = 1.0
+		self.resistance = 1.0
+		self.temperature = 0.0
+		self.delta_temp = 1.0
 
-		self.PicoChannel = 0
-		self.PicoRange = 0
+		self.pico_channel = 0
+		self.pico_range = 0
 
-		self.SetTemp = -1.0
+		self.set_temp = -1.0
 
-		self.TCSHeater = [0,0,0]
-		self.TCSRange = [1,1,1]
-		self.TCSCurrent = [0,0,0]
+		self.tcs_heater = [0,0,0]
+		self.tcs_range = [1,1,1]
+		self.tcs_current = [0,0,0]
 
-		self.MaxSetTemp = 10000.0
-		self.MaxCurrent = 35000
+		self.max_set_temp = 10000.0
+		self.max_current = 35000
 
 		# Acceptable temperature error as a factor e.g. 100 * 0.005 = 0.5mK
-		self.ErrorTemp = 0.01 # The acceptable error in temperature
-		self.ErrorDeltaTemp = 0.005 # The acceptable stability
+		self.error_temp = 0.01 # The acceptable error in temperature
+		self.error_delta_temp = 0.005 # The acceptable stability
 
 		# Sweep description
-		self.SweepFinish = 0.0
-		self.SweepStart = 0.0
-		self.SweepRate = 1.0 # As received from socket in mK/min
-		self.SweepRateSec = 1.0/60.0
-		self.SweepTime = 0.0 # seconds
-		self.SweepDirection = 1.0
-		self.SweepStartTime = 0.0
-		self.SweepTimeLength = 0.0
-		self.SweepMaxOverTime = 15.0 # minutes
+		self.sweep_finish = 0.0
+		self.sweep_start = 0.0
+		self.sweep_rate = 1.0 # As received from socket in mK/min
+		self.sweep_rate_sec = 1.0/60.0
+		self.sweep_time = 0.0 # seconds
+		self.sweep_direction = 1.0
+		self.sweep_start_time = 0.0
+		self.sweep_time_length = 0.0
+		self.sweep_max_over_time = 15.0 # minutes
 
 		# Status parameters
-		self.AtSet = False
-		self.SweepMode = False
-		self.StatusMsg = 0 # not ready
-		self.TempHistory = deque(np.zeros((60,)))
+		self.at_set = False
+		self.sweep_mode = False
+		self.status_msg = 0 # not ready
+		self.temp_history = deque(np.zeros((60,)))
 
 		# Status events
-		self.StatusInterval = 1.0
-		self.LastStatusTime = datetime.now()
-		self.Sensor = "CERNOX"
+		self.status_interval = 1.0
+		self.last_status_time = datetime.now()
+		self.sensor = "CERNOX"
 
 		# Initialize a pid controller
 		self.pid = pid_control.PID(P=20.,I=.5,D=0,Derivator=0,Integrator=0,Integrator_max=60000,Integrator_min=-2000)
 
 		return
 
-	def SetTCS(self,Source,Current):
-		if Current < 0:
-			Current = 0
-		elif Current > self.MaxCurrent:
-			Current = self.MaxCurrent
-		# Current in microAmp
-		# print Current
-		Source = Source + 1
-		command = " ".join(("SETDAC","%d" % Source,"0","%d" % Current))
+	def set_tcs(self,source,current):
+		if current < 0:
+			current = 0
+		elif current > self.max_current:
+			current = self.max_current
+		# current in microAmp
+		# print current
+		source = source + 1
+		command = " ".join(("SETDAC","%d" % source,"0","%d" % current))
 
-		self.TCSVisa.query(command)
+		self.tcs_visa.query(command)
 		return
 
-	def ReadPico(self):
+	def read_pico(self):
 		# Get the resistance of the current channel of the picowatt
-		self.PicoVisa.write("ADC")
+		self.pico_visa.write("ADC")
 		time.sleep(0.45)
-		Answer = self.PicoVisa.query("RES?")
-		Answer = Answer.strip()
+		answer = self.pico_visa.query("RES?")
+		answer = answer.strip()
 		try:
-			self.Resistance = float(Answer)
+			self.resistance = float(answer)
 		except:
-			self.Resistance = self.Resistance
+			self.resistance = self.resistance
 			pass
 		return
 
-	def ReadPicoRange(self):
-		Answer = self.PicoVisa.query("RAN?")
-		Answer = Answer.strip()
-		self.PicoRange = int(Answer)
+	def read_pico_range(self):
+		answer = self.pico_visa.query("RAN?")
+		answer = answer.strip()
+		self.pico_range = int(answer)
 		return
 
-	def SetPicoChannel(self,Channel):
-		self.PicoVisa.write("INP 0")
-		Command = "".join(("MUX ","%d" % Channel))
-		self.PicoVisa.write(Command)
+	def set_pico_channel(self,channel):
+		self.pico_visa.write("INP 0")
+		command = "".join(("MUX ","%d" % channel))
+		self.pico_visa.write(command)
 		time.sleep(3)
-		self.PicoVisa.write("INP 1")
+		self.pico_visa.write("INP 1")
 		time.sleep(10)
-		self.PicoChannel = Channel
+		self.pico_channel = channel
 		return
 
-	def ReadTCS(self):
-		Answer = self.TCSVisa.query("STATUS?")
-		Reply = Answer.split("\t")[1]
-		Reply = Reply.split(",")
-		Range = Reply[1::4]
-		Current = Reply[2::4]
-		Heaters = Reply[3::4]
-		TMP = [1,10,100,1000]
+	def read_tcs(self):
+		answer = self.tcs_visa.query("STATUS?")
+		reply = answer.split("\t")[1]
+		reply = reply.split(",")
+		sensor_range = reply[1::4]
+		current = reply[2::4]
+		heaters = reply[3::4]
+		tmp = [1,10,100,1000]
 		for i in range(3):
-			self.TCSHeater[i] = int(Heaters[i])
+			self.tcs_heater[i] = int(heaters[i])
 		for i in range(3):
-			self.TCSCurrent[i] = int(Current[i])*TMP[int(Range[i])-1]
+			self.tcs_current[i] = int(current[i])*tmp[int(sensor_range[i])-1]
 		return
 
-	def CalcTemperature(self,Calibration,factor=0.0):
-		logR = np.log10(self.Resistance)-factor
-		RPoly = np.ones((len(Calibration),))
-		OldT = self.Temperature
-		for i in range(1,len(RPoly)):
-			RPoly[i] = logR * RPoly[i-1]
-		self.Temperature = np.power(10,(np.sum(np.multiply(RPoly,Calibration))))
-		self.DeltaTemp = self.Temperature - OldT
+	def calc_temperature(self,calibration,factor=0.0):
+		log_resistance = np.log10(self.resistance)-factor
+		r_poly = np.ones((len(calibration),))
+		old_temperature = self.temperature
+		for i in range(1,len(r_poly)):
+			r_poly[i] = log_resistance * r_poly[i-1]
+		self.temperature = np.power(10,(np.sum(np.multiply(r_poly,calibration))))
+		self.delta_temp = self.temperature - old_temperature
 
-		self.TempHistory.pop()
-		self.TempHistory.appendleft(self.Temperature)
+		self.temp_history.pop()
+		self.temp_history.appendleft(self.temperature)
 		return
 
-	# Update the parameter AtSet for the probe
-	def UpdateAtSet(self):
-		Set = False
+	# Update the parameter at_set for the probe
+	def update_at_set(self):
+		is_set = False
 		# The stability measure is v crude
-		Stable = False
+		is_stable = False
 		# 1 = Sweep
-		ErrorFactor = abs(self.Temperature - self.SetTemp)/self.Temperature
-		DeltaTempFactor = abs(np.std(self.TempHistory))/self.Temperature
-		if ErrorFactor < self.ErrorTemp:
-			Set = True
-		if DeltaTempFactor < self.ErrorDeltaTemp:
-			Stable = True
-		self.AtSet = Set and Stable
+		error_factor = abs(self.temperature - self.set_temp)/self.temperature
+		delta_temp_factor = abs(np.std(self.temp_history))/self.temperature
+		if error_factor < self.error_temp:
+			is_set = True
+		if delta_temp_factor < self.error_delta_temp:
+			is_stable = True
+		self.at_set = is_set and is_stable
 		return
 
 	# Interpret a message from the socket, current possible messages are
 	# SET ...  -  set probe the temperature
 	# SWP ...  -  sweep the probe temperature
-	def ReadMsg(self,Msg):
-		Msg = Msg.split(" ")
+	def read_msg(self,msg):
+		msg = msg.split(" ")
 
-		if Msg[0] == "SET":
+		if msg[0] == "SET":
 			try:
-				NewSet = float(Msg[1])
+				new_set_temperature = float(msg[1])
 				# Only interpret new setpoints if the change is >50mK
-				if abs(self.SetTemp-NewSet) > 0.05:
-					self.SetTemp = NewSet
-					if self.PicoChannel == 5:
+				if abs(self.set_temp-new_set_temperature) > 0.05:
+					self.set_temp = new_set_temperature
+					if self.pico_channel == 5:
 						pass
-					#elif self.SetTemp > 800:
+					#elif self.set_temp > 800:
 					#	self.pid.setKp(20.)
 					#else:
 					#	self.pid.setKp(10.)
-					self.pid.setPoint(self.SetTemp)
+					self.pid.initialize_set_point(self.set_temp)
 					# Set at set to be false and write the new set point
-					self.AtSet = False
-					self.SweepMode = False
-					print("Got probe set point from socket %.2f\n" % self.SetTemp)
+					self.at_set = False
+					self.sweep_mode = False
+					print("Got probe set point from socket %.2f\n" % self.set_temp)
 			except:
 				pass
 
-		if Msg[0] == "SWP":
+		if msg[0] == "SWP":
 			try:
-				self.SweepFinish = float(Msg[1])
-				if abs(self.SweepFinish - self.SetTemp) > 0.05:
-					self.SweepStart = self.SetTemp
-					self.pid.setPoint(self.SetTemp)
-					self.SweepRate = abs(float(Msg[2]))
-					self.SweepRateSec = self.SweepRate/60.0
-					self.SweepMaxOverTime = abs(float(Msg[3]))
+				self.sweep_finish = float(msg[1])
+				if abs(self.sweep_finish - self.set_temp) > 0.05:
+					self.sweep_start = self.set_temp
+					self.pid.initialize_set_point(self.set_temp)
+					self.sweep_rate = abs(float(msg[2]))
+					self.sweep_rate_sec = self.sweep_rate/60.0
+					self.sweep_max_over_time = abs(float(msg[3]))
 					# Check if the sweep is up or down
-					if self.SweepFinish >= self.SetTemp:
-						self.SweepDirection = 1.0
+					if self.sweep_finish >= self.set_temp:
+						self.sweep_direction = 1.0
 					else:
-						self.SweepDirection = -1.0
+						self.sweep_direction = -1.0
 					# Put the LS340 into ramp mode
-					self.AtSet = False
-					self.SweepTimeLength = abs(self.SetTemp - self.SweepFinish)/self.SweepRate
-					print("Got temperature sweep to %.2f K at %.2f K/min... Sweep takes %.2f minutes, maximum over time is %.2f" % (self.SweepFinish, self.SweepRate, self.SweepTimeLength, self.SweepMaxOverTime))
+					self.at_set = False
+					self.sweep_time_length = abs(self.set_temp - self.sweep_finish)/self.sweep_rate
+					print("Got temperature sweep to %.2f K at %.2f K/min... Sweep takes %.2f minutes, maximum over time is %.2f" % (self.sweep_finish, self.sweep_rate, self.sweep_time_length, self.sweep_max_over_time))
 					# Write the finish temp
 					# Write the setpoint to start the ramp
-					self.SweepMode = True
-					self.SweepStartTime = datetime.now()
+					self.sweep_mode = True
+					self.sweep_start_time = datetime.now()
 					print("Starting the sweep\n")
 			except:
 				pass
 
-		if Msg[0] == "T_ERROR":
+		if msg[0] == "T_ERROR":
 			try:
-				self.ErrorTemp = float(Msg[1])
+				self.error_temp = float(msg[1])
 			except:
 				pass
 
-		if Msg[0] == "DT_ERROR":
+		if msg[0] == "DT_ERROR":
 			try:
-				self.ErrorDeltaTemp = float(Msg[1])
+				self.error_delta_temp = float(msg[1])
 			except:
 				pass
 
 		return
 
-	def SweepControl(self):
+	def sweep_control(self):
 
 		# We are sweeping so check if the sweep is finished
-		dT = datetime.now() - self.SweepStartTime
+		dT = datetime.now() - self.sweep_start_time
 		dTMin = dT.seconds/60.0
 
-		if dTMin > (self.SweepTimeLength + self.SweepMaxOverTime):
+		if dTMin > (self.sweep_time_length + self.sweep_max_over_time):
 			# The sweep ran out of time, stop it
-			SweepFinished = True
+			sweep_finished = True
 			print("Sweep over time... Finishing...")
-		elif (self.Temperature - self.SweepFinish)*self.SweepDirection > 0.0:
-			SweepFinished = True
+		elif (self.temperature - self.sweep_finish)*self.sweep_direction > 0.0:
+			sweep_finished = True
 			print("Final temperature reached... Finishing...")
 		else:
-			SweepFinished = False
+			sweep_finished = False
 
-		if SweepFinished:
-			self.SweepMode = False
+		if sweep_finished:
+			self.sweep_mode = False
 		else:
-			OldSet = self.SetTemp
-			self.SetTemp = self.SweepStart + self.SweepRateSec * dT.seconds * self.SweepDirection
+			OldSet = self.set_temp
+			self.set_temp = self.sweep_start + self.sweep_rate_sec * dT.seconds * self.sweep_direction
 
-			if self.PicoChannel == 5:
+			if self.pico_channel == 5:
 				pass
-			#elif (OldSet < 800.) and (self.SetTemp > 800.):
+			#elif (OldSet < 800.) and (self.set_temp > 800.):
 			#	self.pid.setKp(20.)
-			#elif (OldSet > 800.) and (self.SetTemp < 800.):
+			#elif (OldSet > 800.) and (self.set_temp < 800.):
 			#	self.pid.setKp(10.)
-			self.pid.setPoint(self.SetTemp,reset=False)
+			self.pid.initialize_set_point(self.set_temp,reset=False)
 
 		return
 
 
 
-	def UpdateStatusMsg(self):
+	def update_status_msg(self):
 		# TDaemon status messages:
 		# 0 = Not ready
 		# 1 = Ready
 
-		if self.AtSet and not self.SweepMode:
-			Status = 1 # Ready
+		if self.at_set and not self.sweep_mode:
+			status = 1 # Ready
 		else:
-			Status = 0 # Not ready
+			status = 0 # Not ready
 
-		self.StatusMsg = Status
+		self.status_msg = status
 		return
 
-	def PrintStatus(self):
-		StatusString = "%s = %.2f K; PID output = %d; " % (self.Sensor,self.Temperature,self.PIDOut)
-		StatusString += "Status message = %d; " % self.StatusMsg
-		StatusString += "P = %.2f, I = %.2f, D = %.2f\n" % (self.pid.P_value,self.pid.I_value,self.pid.D_value)
-		print(StatusString)
-		self.LastStatusTime = datetime.now()
+	def print_status(self):
+		status_string = "%s = %.2f K; PID output = %d; " % (self.sensor,self.temperature,self.pid_output)
+		status_string += "Status message = %d; " % self.status_msg
+		status_string += "P = %.2f, I = %.2f, D = %.2f\n" % (self.pid.P_value,self.pid.I_value,self.pid.D_value)
+		print(status_string)
+		self.last_status_time = datetime.now()
 		return
 
-	def TCSSwitchHeater(self,Heater):
-		CommandVec = np.zeros((12,))
-		CommandVec[2+Heater*4] = 1
-		CommandStr = "SETUP "
-		print("Heater %d Switched %d" % (Heater,int(not self.TCSHeater[Heater])))
-		for i in CommandVec:
-			CommandStr = "".join((CommandStr, "%d," % i))
-		CommandStr = CommandStr[:-1]
-		reply = self.TCSVisa.query(CommandStr)
+	def tcs_switch_heater(self,heater):
+		command_vector = np.zeros((12,))
+		command_vector[2+heater*4] = 1
+		command_string = "SETUP "
+		print("Heater %d Switched %d" % (heater,int(not self.tcs_heater[heater])))
+		for i in command_vector:
+			command_string = "".join((command_string, "%d," % i))
+		command_string = command_string[:-1]
+		reply = self.tcs_visa.query(command_string)
 		return
 
 
-##################### Calibrations
-Calibrations={"SO703":[7318.782092,-13274.53584,10276.68481,
+##################### calibrations
+calibrations={"SO703":[7318.782092,-13274.53584,10276.68481,
 	-4398.202411,1123.561007,-171.3095557,14.43456504,-0.518534965],
 		"SO914":[5795.148097375,-11068.032226486,9072.821104899,
 			-4133.466851312,1129.955799406,-185.318021359,16.881907269,-0.658939155],
@@ -322,63 +322,63 @@ if __name__ == '__main__':
 
 	control = TControl()
 
-	control.SetPicoChannel(5) #ch5 for CERNOX. Do not use below 1K
-	control.Sensor = "CERNOX"
+	control.set_pico_channel(5) #ch5 for CERNOX. Do not use below 1K
+	control.sensor = "CERNOX"
 
 	# Main loop
-	control.ReadTCS()
+	control.read_tcs()
 
 	while True:
 
 		# Read the picowatt and calculate the temperature
-		control.ReadPico()
-		control.CalcTemperature(Calibrations[control.Sensor])
-		control.UpdateAtSet()
-		control.UpdateStatusMsg()
+		control.read_pico()
+		control.calc_temperature(calibrations[control.sensor])
+		control.update_at_set()
+		control.update_status_msg()
 
 		# Push the reading to clients
-		for j in control.Server.handlers:
-			j.to_send = ",%.3f %d" % (control.Temperature, control.StatusMsg)
-			SocketMsg = j.received_data
-			if SocketMsg:
-				control.ReadMsg(SocketMsg)
+		for j in control.server.handlers:
+			j.to_send = ",%.3f %d" % (control.temperature, control.status_msg)
+			socket_msg = j.received_data
+			if socket_msg:
+				control.read_msg(socket_msg)
 		asyncore.loop(count=1,timeout=0.001)
 
 		# if we are sweeping we do some things specific to the sweep
-		if control.SweepMode:
-			control.SweepControl()
+		if control.sweep_mode:
+			control.sweep_control()
 
 		# check if we should send an update
-		UpdateTime = datetime.now() - control.LastStatusTime
-		if UpdateTime.seconds/60.0 >= control.StatusInterval:
-			control.PrintStatus()
+		update_time = datetime.now() - control.last_status_time
+		if update_time.seconds/60.0 >= control.status_interval:
+			control.print_status()
 
-		NEWPID = control.pid.update(control.Temperature)
+		new_pid = control.pid.update(control.temperature)
 		try:
-			control.PIDOut = int(NEWPID)
+			control.pid_output = int(new_pid)
 		except:
-			control.PIDOut = 0
+			control.pid_output = 0
 			pass
 
-		if control.PIDOut < 0:
-			control.PIDOut = 0
-		elif control.PIDOut > control.MaxCurrent:
-			control.PIDOut = control.MaxCurrent
+		if control.pid_output < 0:
+			control.pid_output = 0
+		elif control.pid_output > control.max_current:
+			control.pid_output = control.max_current
 
-		if control.PIDOut > 0 and control.TCSHeater[2] == 0:
+		if control.pid_output > 0 and control.tcs_heater[2] == 0:
 			# status is go to set and heater is off --> turn it on
-			control.SetTCS(2,control.PIDOut)
-			control.TCSSwitchHeater(2)
-			control.ReadTCS()
-		elif control.PIDOut <= 0 and control.TCSHeater[2] == 1:
+			control.set_tcs(2,control.pid_output)
+			control.tcs_switch_heater(2)
+			control.read_tcs()
+		elif control.pid_output <= 0 and control.tcs_heater[2] == 1:
 			# status is go to set and heater is off --> turn it on
-			control.TCSSwitchHeater(2)
-			control.SetTCS(2,0)
-			control.ReadTCS()
-		elif control.PIDOut >= 0 and control.TCSHeater[2] == 1:
-			control.SetTCS(2,control.PIDOut)
-			control.TCSCurrent[2] = control.PIDOut
+			control.tcs_switch_heater(2)
+			control.set_tcs(2,0)
+			control.read_tcs()
+		elif control.pid_output >= 0 and control.tcs_heater[2] == 1:
+			control.set_tcs(2,control.pid_output)
+			control.tcs_current[2] = control.pid_output
 
 		time.sleep(0.5)
 
-	control.TCSVisa.close()
+	control.tcs_visa.close()
