4dac6fe6a84a8a6d96c177aa021f8c27a30151f7
Huang Junye <h.jun.ye@gmail.com>
2019-05-20 18:55:48 +0800

    modify K2002 class to be child class of Keithley class

--- instrument/keithley/k2002.py
+++ instrument/keithley/k2002.py
@@ -1,43 +1,67 @@
 import time
 
-import utils.visa_subs as visa_subs
+from .sourcemeter import Keithley
 
 
-class K2002:
+class K2002(Keithley):
     def __init__(self, address):
+        super().__init__(address)
         self.name = "Keithley 2002"
-        self.address = address
-        self.visa = visa_subs.initialize_gpib(address, 0)
 
-    ######################################
-    # Initializate as voltage source
-    #######################################
+        # Special variables for 2002
+        self.channel = 1
+        self.relative = False
+        self.filter = True
+        self.count = 5
+        self.sense_range = 2.
+        self.auto_sense_range = False
+        self.relative_value = 0.
 
-    def initialize_voltage(
-            self, a_cal=False, relative=False, filt=True,
-            count=5, sense_range=2., auto_range=False
+    def description(self):
+        """Print a description string to data file"""
+
+        description_string = (
+            f"{super().description()}, "
+            f"sense={self.sense}, "
+            f"sense range={self.sense_range}, "
+            f"relative={self.relative}, "
+            f"relative value={self.relative value}, "
+            "\n"
+        )
+        return description_string
+
+    def initialize(
+            self, a_cal=False, relative=False, filter=True,
+            count=5, sense_range=2., auto_sense_range=False
     ):
 
         self.column_names = "V (V)"
         self.data = [0.0]
         self.data_column = 0
         self.sense = "VOLT"
+
         # Special variables for 2002
         self.relative = relative
-        self.filt = filt
+        self.filter = filter
         self.count = count
         self.sense_range = sense_range
-        self.auto_range = auto_range
+        self.auto_sense_range = auto_sense_range
 
-        # A bunch of commands to configure the 2182
+        # A bunch of commands to configure the 2002
         self.visa.write("*RST")
         time.sleep(.1)
         self.visa.write(":SENS:FUNC \'VOLT:DC\'")
         self.visa.query(":READ?")
-        # Disable concurrent mode, measure I and V (not R)
-        self.set_sense_range(sense_range=sense_range, auto_range=auto_range)
 
-        if self.filt:
+        if self.auto_sense_range:
+            self.auto_sense_range = True
+            self.visa.write("".join((":SENS:", self.sense, ":RANG:AUTO 1")))
+        else:
+            self.sense_range = sense_range
+            self.auto_sense_range = False
+            self.visa.write("".join((":SENS:", self.sense, ":RANG ", "%.2e" % sense_range)))
+
+        if self.filter:
             self.visa.write(":SENS:VOLT:AVER:STAT 1")
             self.visa.write(":SENS:VOLT:AVER:COUN %d" % count)
         else:
@@ -55,43 +79,8 @@ class K2002:
 
         pass
 
-    ###########################################
-    # Set the range and compliance
-    #######################################
-
-    def set_sense_range(self, sense_range=0.1, auto_range=False):
-
-        if auto_range:
-            self.auto_range = True
-            self.visa.write("".join((":SENS:", self.sense, ":RANG:AUTO 1")))
-        else:
-            self.sense_range = sense_range
-            self.auto_range = False
-            self.visa.write("".join((":SENS:", self.sense, ":RANG ", "%.2e" % sense_range)))
-
-        pass
-
-    ##################################################
-    # Read data
-    ################################################
-
     def read_data(self):
         self.visa.write(":INIT")
         reply = self.visa.query(":FETC?")
         self.data = [float(reply)]
         pass
-
-    ###############################################
-    # Print a description string
-    ################################################
-
-    def description(self):
-        description_string = "Keithley2002"
-        for item in list(vars(self).items()):
-            if item[0] == "address":
-                description_string = ", ".join((description_string, "%s = %.3f" % item))
-            elif item[0] == "sense" or item[0] == "sense_range" or item[0] == "relative" or item[0] == "relative_value":
-                description_string = ", ".join((description_string, "%s = %s" % item))
-
-        description_string = "".join((description_string, "\n"))
-        return description_string
